937d743dee86427ddd971ecaa55e20b578002d95
Alina <malinka775@yandex.ru>
2024-01-16 21:58:56 +0200

    feat: add test db

---
+++ src/api/db.js
@@ -0,0 +1,12 @@
+export const words = {
+  appearence: [
+    'eyebrow', 'eyelash', 'fair', 'fat', 'frown', 'ginger', 'good-looking', 'gorgeous', 'gray-haired', 'grin', 'hair', 'handsome', 'height', 'hideous', 'lipstick', 'make-up', 'moustache', 'obese', 'obesity', 'plain', 'posture', 'pretty', 
+     'skinny', 'slim', 'tanned', 'ugly', 'unattractive', 'wavy'],
+  colors: [
+    'black', 'blue', 'bright', 'brown', 'color', 'dark', 'ginger', 'golden', 'green', 'pink', 'purple', 'shade', 'vivid', 'white', 'yellow',
+  ],
+  family: [
+    'aunt', 'brother', 'brother-in-law', 'cousin', 'dad', 'daughter', 'daughter-in-law', 'eldest', 'family', 'father', 'father-in-law', 'grandchild', 'granddaughter', 'grandfather', 'grandkid', 'grandma', 'grandmother', 'grandpa', 'grandparent', 'grandson', 'granny', 'husband', 'in-laws', 'mom', 
+    'mother', 'mother-in-law', 'nephew', 'niece', 'parent', 'parents-in-law', 'sibling', 'sister', 'sister-in-law', 'son', 'son-in-law', 'twin', 'uncle', 'wife',
+  ],
+}
